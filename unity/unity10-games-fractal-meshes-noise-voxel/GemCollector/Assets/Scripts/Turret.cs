17a4a68421da74604a4f7fa61c2340219b6b3254
Muhamad Yasir Noval <myasirnoval@gmail.com>
2026-02-10 14:56:03 +0700

    menambahkan material

---
+++ unity/unity10-games-fractal-meshes-noise-voxel/GemCollector/Assets/Scripts/Turret.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Numerics;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject claw = null;
+    public Animator animatorControllerGun = null;
+
+    private bool isShooting = false;
+    private LayerMask currentMask;
+
+    public bool IsShooting
+    {
+        get
+        {
+            return isShooting;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && !isShooting)
+        {
+            LaunchClaw();
+        }
+    }
+
+    private void LaunchClaw()
+    {
+        isShooting = true;
+
+        if (animatorControllerGun != null)
+        {
+            animatorControllerGun.speed = 0f;
+
+            RaycastHit hit;
+            Vector3 down = transform.TransformDirection(Vector3.down);
+        }
+    }
+}
